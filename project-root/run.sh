c47e39a7ad3a2f367ec7a451c3928389d2da888c
OhanyanDavit <ohanyandavit556@gmail.com>
2025-06-30 15:23:39 +0400

    Update

--- project-root/run.sh
+++ project-root/run.sh
@@ -2,19 +2,19 @@
 
 echo "🚀 Starting all services..."
 
-# Запуск student_service
+
 (cd student_service  && node server.js &)
 STUDENT_PID=$!
 
-# Запуск course_service
+
 (cd course_service  && node server.js &)
 COURSE_PID=$!
 
-# Запуск registration_service
+
 (cd registration_service  && node server.js &)
 REGISTRATION_PID=$!
 
-# Запуск или перезапуск NGINX
+
 if nginx -t 2>/dev/null; then
     echo "✅ Reloading NGINX..."
     sudo nginx -s reload 
@@ -24,9 +24,9 @@ else
 
 fi
 
-# Обработка сигналов для остановки всех сервисов
+
 
 trap 'echo "🛑 Stopping..."; kill $STUDENT_PID $COURSE_PID $REGISTRATION_PID; sudo nginx -s stop; exit' SIGINT SIGTERM
 
-# Цикл ожидания (Ctrl+C для завершения)
+
 while true; do sleep 1; done
